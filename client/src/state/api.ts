64c13cc5e4abd13f3dd08f1397754e69a60bb38f
Rezoan Shakil Prince <rezoan.shakil@bjitgroup.com>
2025-11-12 16:47:09 +0600

    Schema understanding

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,58 @@
+import { Manager, Tenant } from '@/types/prismaTypes';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    // Automatically attach Authorization header with ID token of any API request
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession();
+      const { idToken } = session.tokens ?? {};
+
+      if (idToken) {
+        headers.set('Authorization', `Bearer ${idToken}`);
+      }
+
+      return headers;
+    },
   }),
   reducerPath: 'api',
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    // build.query<Receive Type, Sending Type>
+    getAuthUser: build.query<User, void>({
+      queryFn: async (_, _queryApi, _extraOptions, fetchWithBQ) => {
+        try {
+          const session = await fetchAuthSession();
+          const { idToken } = session.tokens ?? {};
+          const userRole = idToken?.payload['custom:role'] as string;
+
+          const user = await getCurrentUser();
+
+          const endpoints =
+            userRole === 'manager'
+              ? `/managers/${user.userId}`
+              : `/tenants/${user.userId}`;
+
+          let userDetailsResponse = await fetchWithBQ(endpoints);
+
+          // If user doen't exist, create a new user
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailsResponse.data as Tenant | Manager,
+              userRole,
+            },
+          };
+        } catch (error: any) {
+          return {
+            error: error.message || 'Could not fetch authenticated user',
+          };
+        }
+      },
+    }),
+  }),
 });
 
 export const {} = api;
